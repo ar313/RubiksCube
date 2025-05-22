cca2637489b0e0bf9559a41061d45ea27e949d85
AhmedR <ahmed@AR313>
2025-05-22 12:31:56 +0300

    fix typo in environment

--- RubiksCubeUI/src/app/core/services/rubikscube.service.ts
+++ RubiksCubeUI/src/app/core/services/rubikscube.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RubiksCubeDto } from '../../models/rubikscube/rubikscubeDto.model';
-import { environment } from '../../../../envirionment';
+import { environment } from '../../../../environment';
 import { RotateCubeDto } from '../../models/rubikscube/rotateCubeDto.model';
 
 @Injectable({ providedIn: 'root' })
